560352ca581537cf78cd5297467de5427fa77689
unknown <sumitrohilla171996@gmail.com>
2024-06-18 23:29:02 +0530

    python version update for django

--- build_files.sh
+++ build_files.sh
@@ -3,9 +3,9 @@
 echo "BUILD START"
 
 # Install dependencies
-python3.9 -m pip install -r requirements.txt
+python3.12.4 -m pip install -r requirements.txt
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput --clear
+python3.12.4 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
