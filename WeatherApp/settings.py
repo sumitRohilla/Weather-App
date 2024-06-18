b00bcf966f58d4647b948f619360460226672738
unknown <sumitrohilla171996@gmail.com>
2024-06-19 00:56:51 +0530

    index update

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+import os
+
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR, "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
