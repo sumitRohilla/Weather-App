4909b8d7157ffde3266f797817acea9a25a2f5b4
unknown <sumitrohilla171996@gmail.com>
2024-06-18 23:14:10 +0530

    build_files updated

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,14 @@
-# build_files.sh
+#!/bin/bash
 
 echo "BUILD START"
+
+# Install pip
+apt-get update && apt-get install -y python3-pip
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
